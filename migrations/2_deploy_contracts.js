d5bfb67de8431ef80fe003a4494b273b30e40479
hi-jeeyong <jeeyongyun@hiblocks.io>
2021-04-14 11:01:44 +0900

    수정

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,9 +5,9 @@ const Market = artifacts.require('CojamMarket');
 module.exports = function(deployer, network, account) {
   let token;
     deployer.then( async ()=>{
-      token = await deployer.deploy(Token);
+      //token = await deployer.deploy(Token);
     //}).then( async ()=>{
-      //let market = await deployer.deploy(Market, process.env.TESTNET_CONTRACT_ADDRESS); //테스트넷
+      let market = await deployer.deploy(Market, process.env.TESTNET_CONTRACT_ADDRESS); //테스트넷
       //let market = await deployer.deploy(Market, process.env.MAINNET_CONTRACT_ADDRESS); //메인넷
     });
 }
