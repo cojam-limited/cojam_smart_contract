e36b1626d825e3848726c5ec480e7eb3bee7e5e0
hi-jeeyong <jeeyongyun@hiblocks.io>
2021-07-29 00:30:41 +0900

    현재 오너확인 추가

--- scripts/changeOwner.js
+++ scripts/changeOwner.js
@@ -12,13 +12,29 @@ async function changeOwner(newOwner) {
                            :process.env.PRIVATE_KEY_CYPRESS;
 
   caver.klay.accounts.wallet.add(privateKey);
-  const owner = new caver.klay.Contract(Owner.abi, '0x4f508e75F2Cf4F1c785daEf4A63BE9708c4B3443');
+  const owner = new caver.klay.Contract(Owner.abi, '0x5C4fDBD36cf38e4794951254D4285b53f545e2B4');
 
   const from = caver.klay.accounts.wallet.getAccount(0).address;
   const res = await owner.methods.transferOwnership(newOwner).send({ from: from, gas: 100000 });
   console.log(res);
 }
 
-changeOwner('0x4a1667cf934796067adbddf456c95ef91658b403');
+async function currentOwner() {
+  const caver = IS_DEV?(new Caver('https://api.baobab.klaytn.net:8651'))
+                      :(new Caver("https://kaikas.cypress.klaytn.net:8651"));
+
+  // TODO: enter the private key of token owner
+  const privateKey = IS_DEV?process.env.PRIVATE_KEY
+                           :process.env.PRIVATE_KEY_CYPRESS;
+
+  caver.klay.accounts.wallet.add(privateKey);
+  const owner = new caver.klay.Contract(Owner.abi, '0x5C4fDBD36cf38e4794951254D4285b53f545e2B4');
+
+  const res = await owner.methods.owner().call();
+  console.log(res);
+}
+
+//changeOwner('0x281650e1270265dde07ae465d179eb560132eafa');
+currentOwner();
 
 // 실행 - node ./scripts/changeOwner.js
